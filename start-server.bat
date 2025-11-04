8ce3f2434cad7746425a75677563785dd2866f2d
PieroHugo <167128323+PieroHugo@users.noreply.github.com>
2025-11-04 13:21:38 -0500

    docs: delete files

--- start-server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ========================================
-echo   INICIANDO EASYPARK PLATFORM
-echo ========================================
-echo.
-cd /d "C:\Users\WINDOWS 10\IdeaProjects\platform"
-.\mvnw.cmd spring-boot:run
-
