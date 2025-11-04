6ad4edcce0c1bc696780e65e0c9b1ae3afb2a838
Piero Elescano <pieroelescano300@gmail.com>
2025-11-04 00:29:15 -0500

    test: add integration test for Spring Boot application context

---
+++ src/test/java/com/easypark/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easypark.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
