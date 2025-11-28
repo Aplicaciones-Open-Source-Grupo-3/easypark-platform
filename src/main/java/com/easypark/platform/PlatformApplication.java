3814a76a4229cf679865480255fb71eac50443d2
Piero Elescano <pieroelescano300@gmail.com>
2025-11-28 13:09:43 -0500

    feat: update time zone

--- src/main/java/com/easypark/platform/PlatformApplication.java
+++ src/main/java/com/easypark/platform/PlatformApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class PlatformApplication {
 
     public static void main(String[] args) {
+        // Configurar zona horaria de Perú para toda la aplicación
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
         SpringApplication.run(PlatformApplication.class, args);
     }
 }
